579a0956f50198cb8bdcd33a7bf49e5636f55ef1
oebaedz <oebaedz@gmail.com>
2024-05-31 10:51:37 +0700

    Connecting FE to BE

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,33 @@
-import { useState } from 'react'
+import { useEffect, useState } from "react";
+import Cards from "./Components/Cards";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [bookmark, setBookmark] = useState('')
+
+  const getData = async() => {
+    const res = await fetch('http://localhost:1999/')
+    const mark = await res.json()
+    setBookmark(mark)
+  }
+  // console.log(bookmark)
+  useEffect(() => {
+    getData()
+  }, [])
+  
 
   return (
     <>
-      <h1 className='text-3xl bg-green-400'>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="w-full flex flex-col">
+        <h1 className="text-3xl bg-green-400 text-center font-bold py-3">
+          Bookmark Kajian Al-Insijam
+        </h1>
+        {/* {bookmark.map(a=> "a")} */}
+        <div className="flex flex-col my-4 items-center">
+          <Cards bookmark={bookmark}/>
+        </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const supabase = require('@supabase/supabase-js')
+const cors = require('cors')
 
 const app = express()
 const PORT = 1999 || process.env.PORT
 app.use(express.json())
+app.use(cors())
 
 const SUPA_URL = 'https://mjehoozrgrrlagkypxiv.supabase.co'
 const SUPA_SERVICE_ROLE = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1qZWhvb3pyZ3JybGFna3lweGl2Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcxNjYzMTAwOSwiZXhwIjoyMDMyMjA3MDA5fQ.WjRs9yixZ3UlXJ98uqsutaNsK3edbEqniq6Sgf7eavs'
